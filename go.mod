7c9f33ca7e4be12fe7ffaa3b2cd7c5e918ef1017
charles burton <charles.burton@cybergrx.com>
2025-07-11 16:26:51 -0400

    feat: bringup addon support

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/charles-d-burton/hobor
 
 go 1.24.3
+
+require (
+	github.com/fxamacker/cbor/v2 v2.7.1-0.20240908165221-a01272272f50 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"log"
+	"log/slog"
 	"net/http"
 
-	"github.com/charles-d-burton/homebor/hapi"
+	"github.com/charles-d-burton/hobor/hapi"
+	"github.com/fxamacker/cbor/v2"
 )
 
 func main() {
@@ -11,5 +14,21 @@ func main() {
 	mux.HandleFunc("GET "+hapi.API, func(w http.ResponseWriter, r *http.Request) {
 		// TODO: Implement the logic to get from th ehome assistant API
 		getAPI := hapi.GetAPIMessage{Message: "API running."}
+		// payload, err := io.ReadAll(r.Body)
+		// if err != nil {
+		// 	slog.Error("unable to decode messge body", "error", err)
+		// }
+		encoded, err := cbor.Marshal(getAPI)
+		if err != nil {
+			slog.Error("unable to marshal cbor getAPI", "error", err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/cbor")
+
+		_, err = w.Write(encoded)
+		if err != nil {
+			slog.Error("unable to write encoded message", "error", err)
+		}
 	})
+	log.Fatal(http.ListenAndServe(":9007", mux))
 }
